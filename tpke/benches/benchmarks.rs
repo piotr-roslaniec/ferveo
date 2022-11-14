97eeb1ac80192f3edecff8759cc1f73617f8748e
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-11-14 11:46:39 +0100

    benchmark size of primitives in various dkg configs

--- tpke/benches/benchmarks.rs
+++ tpke/benches/benchmarks.rs
@@ -20,7 +20,7 @@ pub fn bench_decryption(c: &mut Criterion) {
         let threshold = num_shares * 2 / 3;
 
         let (pubkey, _, contexts) =
-            setup::<E>(threshold, num_shares, num_entities);
+            setup::<E>(threshold, num_shares, num_entities, false);
 
         // let mut messages: Vec<[u8; NUM_OF_TX]> = vec![];
         let mut messages: Vec<Vec<u8>> = vec![];
@@ -70,7 +70,7 @@ pub fn bench_decryption(c: &mut Criterion) {
         let threshold = num_shares * 2 / 3;
 
         let (pubkey, _, contexts) =
-            setup::<E>(threshold, num_shares, num_entities);
+            setup::<E>(threshold, num_shares, num_entities, false);
 
         // let mut messages: Vec<[u8; NUM_OF_TX]> = vec![];
         let mut messages: Vec<Vec<u8>> = vec![];
